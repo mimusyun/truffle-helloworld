037eaa2ef1841a7df1ede7533e0092c58bd17170
Mimura Shunichiro <mimuramimusyun@gmail.com>
2021-12-30 18:44:32 +0100

    helloworld

---
+++ contracts/Helloworld.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+contract Helloworld {
+
+    string public message;
+    address public owner;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    function setMessage(string memory _message) public payable {
+        require(msg.value >= 1 ether);
+        message = _message;
+        owner = msg.sender;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+}
